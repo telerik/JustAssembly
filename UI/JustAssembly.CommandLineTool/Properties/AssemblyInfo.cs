9441522705a10d08ed0b98faf3bf2d0328e40d51
Alexander Dimitrov <alexander.dimitrov@telerik.com>
2017-05-16 17:03:23 +0300

    JustAssembly.CommandLineTool now includes CommonAssemblyInfo.cs and CompanyAssemblyInfo.cs as links.

--- UI/JustAssembly.CommandLineTool/Properties/AssemblyInfo.cs
+++ UI/JustAssembly.CommandLineTool/Properties/AssemblyInfo.cs
@@ -8,9 +8,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("JustAssembly.CommandLineTool")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("JustAssembly.CommandLineTool")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,16 +18,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("c6d6187d-cb83-40a1-a3f6-20fb9231d67b")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
